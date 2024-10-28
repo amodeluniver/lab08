33ca9fd740b1d1c9e99beb6e431124b18cb1bf87
Anthony Peña <effrenanthony2704@gmail.com>
2024-10-28 18:07:08 -0500

    sape

--- src/lab08_1/Main.java
+++ src/lab08_1/Main.java
@@ -12,6 +12,11 @@ public class Main {
     public static void main(String[] args) {
         for(int f = 0; f <= 150; f += 10) {
             System.out.println(f + " degrees Fahrenheit is " + ((f - 32) * 5 / 9) + " degrees Celsius");
+            System.out.println(f + " degrees Fahrenheit is " + ((f - 32) * 5 / 9) + " degrees Celsius");
+            System.out.println(f + " degrees Fahrenheit is " + ((f - 32) * 5 / 9) + " degrees Celsius");
+            System.out.println(f + " degrees Fahrenheit is " + ((f - 32) * 5 / 9) + " degrees Celsius");
+            System.out.println(f + " degrees Fahrenheit is " + ((f - 32) * 5 / 9) + " degrees Celsius");
+            System.out.println(f + " degrees Fahrenheit is " + ((f - 32) * 5 / 9) + " degrees Celsius");
         }
     }
 }
